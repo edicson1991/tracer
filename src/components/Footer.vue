fd61e0e1b3287b609136192cf094414b945c4251
edicson1991 <52640992+edicson1991@users.noreply.github.com>
2022-07-13 08:31:08 -0500

    Primera version
    Descargar e installar npm

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <footer>
+      <p>Todos los derechos reservados | 2022 ITM Consulting</p>
+    </footer>
+  </div>
+</template>
+<style scoped>
+footer {
+  position: fixed;
+  height: 50px;
+  bottom: 0;
+  width: 100%;
+  background-color: #ffffff;
+  color: blanchedalmond;
+}
+
+footer p {
+    
+    color: black;
+}
+</style>
